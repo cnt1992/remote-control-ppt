ff916594b5562c60a90791c2b908714a8ba39de6
cnt1992 <418231398@qq.com>
2015-08-21 12:20:59 +0800

    初始化项目

---
+++ config/index.js
@@ -0,0 +1,24 @@
+var config = {
+	presentations : {
+		'reactNative' : {
+			id : 'reactNative',
+			title : 'react-native 实践与总结',
+			icoURL:'/img/react.ico',
+      isActive : false
+		},
+		'es6' : {
+			id : 'es6',
+			title : 'ECMA6 技术分享',
+			icoURL:'/img/es6.ico',
+      isActive : false
+		},
+		'controller' : {
+			title: 'Remote Presentation Controller', 
+			icoURL:'/img/react.ico',
+			layout: "controller_layout",
+      isActive : false
+		}
+	}
+};
+
+exports.config = config;
